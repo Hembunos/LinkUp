2381b355b2a63f7eaf00ac5b77d8e22f007ed4bc
Hembunos <hembunospro1428@gmail.com>
2025-01-02 20:13:45 +0530

    sidebar complete

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -32,11 +32,11 @@ export const onBoardUser = async (req, res, next) => {
     }
 
     const prisma = getPrismaInstance();
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: { email, name, about, profilePicture },
     });
 
-    return res.json({ msg: "Success", status: true });
+    return res.json({ msg: "Success", status: true, user});
 
 
   } catch (error) {
